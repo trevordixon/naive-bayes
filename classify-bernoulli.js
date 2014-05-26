f3605b6a21d3df9b2c8956854614b330dc0483c0
Trevor Dixon <tdixon@adobe.com>
2014-05-26 11:55:25 -0600

    Add comment about smoothing.

--- classify-bernoulli.js
+++ classify-bernoulli.js
@@ -21,6 +21,17 @@ module.exports = function makeClassifier(categories) {
       // For each word, multiply by P(word|c)
       for (var word in words) {
         var docsThatContainWord = cWords[word] || 0;
+
+      /*
+        "Laplacian correction" here. (From http://www.cs.nyu.edu/faculty/davise/ai/bayesText.html)
+
+          Let CW be the number of documents in C that contain at least one occurrence of W. 
+          Let V be the number of different words in D. 
+          Let |C| be the number of documents in the category C. 
+          Then estimate Prob(W|C) as (1+CW)/(|C|+V).
+         
+        I'm not sure, but I think this counts as "smoothing" as mentioned in the requirements.
+      */
         p += Math.log((docsThatContainWord+1) / (docsInCategory+wordsInDocument));
       }
 
